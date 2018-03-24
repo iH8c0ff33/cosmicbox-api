47aac9ac31fc9cd57bc8666945469c699c1f9fea
Daniele Monteleone <daniele@monteleone.ml>
2018-03-24 21:09:03 +0100

    Make fields required

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,9 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CosmicBox.Models {
     public class Event {
+        [Key]
         public long Id { get; set; }
-        public DateTime Timestamp { get; set; }
-        public double Pressure { get; set; }
+
+
+        [Required]
+        public DateTime? Timestamp { get; set; }
+
+        [Required]
+        public double? Pressure { get; set; }
     }
 }
