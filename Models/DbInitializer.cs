3f778711c51d48a132576cc07add12bb2403dd2a
Daniele Monteleone <daniele@monteleone.ml>
2018-04-14 11:42:29 +0200

    Revert "Initialize database on Main"
    This reverts commit 7c4376869eb7fbfad2441aaa75da5cff8f9603e5.

--- Models/DbInitializer.cs
+++
@@ -1,8 +0,0 @@
-namespace CosmicBox.Models {
-    public static class DbInitializer {
-        public static void Initialize(ApiContext context) {
-            context.Database.EnsureCreated();
-            context.SaveChanges();
-        }
-    }
-}
