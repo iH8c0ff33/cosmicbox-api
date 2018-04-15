d2ad8a5e1618bf5b1ea87f902e7d6c281994aa37
Daniele Monteleone <daniele@monteleone.ml>
2018-04-15 11:59:09 +0200

    Add EF migrations

---
+++ Migrations/20180415092040_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CosmicBox.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "boxes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    uuid = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_boxes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "runs",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    start = table.Column<DateTime>(nullable: false),
+                    end = table.Column<DateTime>(nullable: false),
+                    box_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_runs", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_runs_boxes_box_id",
+                        column: x => x.box_id,
+                        principalTable: "boxes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "traces",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    timestamp = table.Column<DateTime>(nullable: false),
+                    pressure = table.Column<float>(nullable: false),
+                    run_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_traces", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_traces_runs_run_id",
+                        column: x => x.run_id,
+                        principalTable: "runs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_runs_box_id",
+                table: "runs",
+                column: "box_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_traces_run_id",
+                table: "traces",
+                column: "run_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "traces");
+
+            migrationBuilder.DropTable(
+                name: "runs");
+
+            migrationBuilder.DropTable(
+                name: "boxes");
+        }
+    }
+}
