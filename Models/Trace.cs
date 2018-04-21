c2a886ac591ad0cccd90c425cfc1732a76a76de6
Daniele Monteleone <daniele@monteleone.ml>
2018-04-21 18:36:57 +0200

    Fix JSON parsing on Trace model

--- Models/Trace.cs
+++ Models/Trace.cs
@@ -1,18 +1,20 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace CosmicBox.Models {
     [Table("traces")]
     public class Trace {
         [Column("id")]
         public int Id { get; set; }
-        [Column("timestamp")]
+        [Column("timestamp"), JsonRequired]
         public DateTime Timestamp { get; set; }
-        [Column("pressure")]
+        [Column("pressure"), JsonRequired]
         public float Pressure { get; set; }
 
-        [Column("run_id")]
+        [Column("run_id"), JsonRequired]
         public int RunId { get; set; }
+        [JsonIgnore]
         public Run Run { get; set; }
     }
 }
