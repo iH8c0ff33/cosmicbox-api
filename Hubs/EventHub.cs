85b99efba0d279fbc169754acb11c3154f5fbe40
Daniele Monteleone <daniele@monteleone.ml>
2018-04-28 23:02:52 +0200

    Implement EventHub

--- Hubs/EventHub.cs
+++ Hubs/EventHub.cs
@@ -1,14 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using CosmicBox.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace CosmicBox.Hubs {
+    [Authorize]
     public class EventHub : Hub {
-        [Authorize("write:events")]
-        public Task SendEvent(string ev) {
+        private readonly CosmicContext _context;
 
-            return Clients.All.SendAsync("Event", ev);
+        public EventHub(CosmicContext context) => _context = context;
+
+        public async Task<IEnumerable<Run>> ListRuns(int boxId) =>
+            await _context.Runs.Where(r => r.BoxId == boxId).ToListAsync();
+
+        public async Task JoinRun(int id) => await Groups.AddAsync(Context.ConnectionId, id.ToString());
+
+        public async Task<bool> AddTrace(Models.Trace trace) {
+            var run = _context.Runs.Where(r => r.Id == trace.RunId).Include(r => r.Box.Grants).FirstOrDefault();
+            if (run == null || !run.Box.HasWriteAccess(Context.UserIdentifier)) {
+                return false;
+            }
+            if (run.End <= DateTime.Now) {
+                return false;
+            }
+
+            _context.Traces.Add(trace);
+            _context.SaveChanges();
+
+            await Clients.Group(run.Id.ToString()).SendAsync("Trace", trace);
+            return true;
         }
     }
 }
